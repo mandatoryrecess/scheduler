20fe35ff421e3e2a571cfb105e8271ca381b66f2
Rossanne Clamp <ross@Rossannes-MacBook-Pro.local>
2021-07-06 18:29:26 -0700

    appointments added

---
+++ src/components/Appointment/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.scss";
+import Header from "./Header"
+
+export default function Appointment(props) {
+
+
+  return (
+    <article className="appointment">Hello</article>
+  );
+}
+

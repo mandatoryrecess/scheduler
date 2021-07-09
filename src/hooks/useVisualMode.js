91b71abe3c4209982461fb6a8af43b701c9951f9
Rossanne Clamp <ross@Rossannes-MacBook-Pro.local>
2021-07-09 13:21:32 -0700

    useVisualMode hook and test

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const useVisualMode = function (initial) {
+
+  const [history, setHistory] = useState([initial]);
+
+
+  const transition = function (newMode, replace = false) {
+    
+    setHistory((previous) => { 
+      if(replace) {
+       const newHistory = [...previous]
+        newHistory.pop();
+        newHistory.push(newMode)
+        return newHistory; 
+      }
+
+    return[...previous, newMode]
+
+
+    
+    });
+  };
+
+  const back = function () {
+
+    if(history.length < 2) {
+      return 
+    }
+
+    setHistory((previous) => {
+      const newHistory = [...previous];
+      newHistory.pop();
+    
+
+      return [...previous.slice(0, previous.length - 1)];
+    });
+  };
+
+  const mode = history.slice(-1)[0];
+  return { mode, transition, back };
+};
+
+export default useVisualMode;
